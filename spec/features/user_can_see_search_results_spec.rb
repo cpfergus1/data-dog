892238714e31119267b8d5925ac05d749bb0aa8b
Dione Wilson <dionew1@gmail.com>
2020-12-07 08:15:20 -0700

    Pass tests for search

--- spec/features/user_can_see_search_results_spec.rb
+++ spec/features/user_can_see_search_results_spec.rb
@@ -5,17 +5,15 @@ describe 'user can search for dog images' do
 
     visit root_path
 
-    fill_in :search, with: "5"
+    fill_in :number, with: "5"
 
     click_button 'Fetch Dogs'
 
-    expect(path).to eq(search_path)
+    expect(current_path).to eq(search_path)
     expect(page).to have_content("5 Results")
 
-    within('.result').first do
+    within(first('.dog')) do
       expect(page).to have_css('.image')
-      image = find('.image')
-      expect(image).to_not be_empty
 
       expect(page).to have_css('.breed')
       breed = find('.breed').text
