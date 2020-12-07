20b19624a36b7398d4b2f346571dab4ffd8185cc
Dione Wilson <dionew1@gmail.com>
2020-12-07 14:43:19 -0700

    Update service and facade spec

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -5,6 +5,7 @@ describe 'Search Facade' do
     dogs = SearchFacade.dog_images(1)
 
     expect(dogs).to be_an(Array)
+    expect(dogs.size).to eq(1)
     expect(dogs.first).to be_an_instance_of(Dog)
   end
 end
--- spec/services/dog_service_spec.rb
+++ spec/services/dog_service_spec.rb
@@ -6,13 +6,22 @@ describe 'Dog Service' do
 
     expect(response).to be_an(Array)
     expect(response[0]).to be_a(Hash)
+
     expect(response[0]).to have_key(:url)
+    expect(response[0][:url]).to be_a(String)
+
     expect(response[0]).to have_key(:breeds)
-    
+    expect(response[0][:breeds]).to be_an(Array)
+
     if !response[0][:breeds].empty?
       expect(response[0][:breeds][0]).to have_key(:name)
+      expect(response[0][:breeds][0][:name]).to be_a(String)
+
       expect(response[0][:breeds][0]).to have_key(:life_span)
+      expect(response[0][:breeds][0][:life_span]).to be_a(String)
+
       expect(response[0][:breeds][0]).to have_key(:temperament)
+      expect(response[0][:breeds][0][:temperament]).to be_a(String)
     end
   end
 end
