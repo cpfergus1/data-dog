4ddf71b72a96140fd52f6a0de9b928de11a0df1a
Dione Wilson <dionew1@gmail.com>
2020-12-07 11:59:59 -0700

    Add search facade spec

---
+++ spec/facades/search_facade_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'Search Facade' do
+  it 'returns dogs' do
+    dogs = SearchFacade.dog_images(1)
+
+    expect(dogs).to be_an(Array)
+    expect(dogs.first).to be_an_instance_of(Dog)
+  end
+end
